cc959233afdc55a4274aa1b36b40781b6d950cce
LujainBhais <lujainbhais716@gmail.com>
2025-04-12 13:34:32 +0300

    Methods

---
+++ Methods/Main.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+public class Main {
+    public static int sumFirstAndLast(int number){
+        if (number <0){
+            return -1;
+        }
+        int lastNumber =number%10;
+        int firstNumber = number;
+        while(firstNumber>=10){
+            firstNumber/=10;
+
+        }
+        return firstNumber+lastNumber;
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter a number to sum First number and Last number : ");
+        Scanner input=new Scanner (System.in);
+        int num=input.nextInt();
+        int result =sumFirstAndLast(num);
+
+        if (result ==-1){
+            System.out.print("Negative number detected! ");
+        }
+        else
+            System.out.print("Sum of first and last numbers are "+result);
+    }
+}
